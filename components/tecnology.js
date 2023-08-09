51df10ae58eb0dba40ce39a65aa218c1863fa891
David Wexler <codigodavid199@gmail.com>
2023-08-09 17:20:16 -0300

    desoptimizacion img

--- components/tecnology.js
+++ components/tecnology.js
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Navigation } from "swiper";
 import { FaHtml5, FaCss3Alt, FaReact, FaJs, FaLaravel, FaNode } from "react-icons/fa";
@@ -14,98 +14,98 @@ const Tecnology = () => {
 
     useEffect(() => {
         setDomLoaded(true);
-      }, []);
+    }, []);
     return (
         <div className={styles.contenedor}>
             <h2 className={styles.titulo}>Tecnologías</h2>
             <div className={styles.tecnologia}>
-            {domLoaded && (
-                <Swiper
-                    spaceBetween={30}
-                    centeredSlides={true}
-                    loop={true}
-                      autoplay={{
-                        delay: 1000,
-                        disableOnInteraction: false,
-                        pauseOnMouseEnter: true,
-                      }}
-                    navigation={true}
-                    modules={[Autoplay, Navigation]}
-                    className="mySwiper"
-                    grabCursor={true}
-                    breakpoints={{
-                        0: {
-                          slidesPerView: 1,
-                          spaceBetween: 10
-                        },
-                        480: {
-                          slidesPerView: 2,
-                          spaceBetween: 10
-                        },
-                        768: {
-                          slidesPerView: 3,
-                          spaceBetween: 15
-                        },
-                        1024: {
-                          slidesPerView: 4,
-                          spaceBetween: 15
-                        },
-                        1280: {
-                          slidesPerView: 5,
-                          spaceBetween: 30
-                        }
-                    }}
-                >
-                    <SwiperSlide>
-                        <div className={styles.tarjeta}>
-                            <p className={styles.paragraph}>
-                                <span className={styles.tooltip} mensaje="Html5"><FaHtml5 /></span>
-                            </p>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className={styles.tarjeta}>
-                            <p className={styles.paragraph}>
-                                <span className={styles.tooltip} mensaje="Css3"><FaCss3Alt /></span>
-                            </p>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className={styles.tarjeta}>
-                            <p className={styles.paragraph}>
-                                <span className={styles.tooltip} mensaje="Javascript"><FaJs /></span>
-                            </p>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className={styles.tarjeta}>
-                            <p className={styles.paragraph}>
-                                <span className={styles.tooltip} mensaje="React"><FaReact /></span>
-                            </p>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className={styles.tarjeta}>
-                            <p className={styles.paragraph}>
-                                <span className={styles.tooltip} mensaje="Laravel"><FaLaravel /></span>
-                            </p>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className={styles.tarjeta}>
-                            <p className={styles.paragraph}>
-                                <span className={styles.tooltip} mensaje="Mysql"><DiMysql /></span>
-                            </p>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className={styles.tarjeta}>
-                            <p className={styles.paragraph}>
-                                <span className={styles.tooltip} mensaje="NodeJS"><FaNode /></span>
-                            </p>
-                        </div>
-                    </SwiperSlide>
-                </Swiper>
+                {domLoaded && (
+                    <Swiper
+                        spaceBetween={30}
+                        centeredSlides={true}
+                        loop={true}
+                        autoplay={{
+                            delay: 1000,
+                            disableOnInteraction: false,
+                            pauseOnMouseEnter: true,
+                        }}
+                        navigation={true}
+                        modules={[Autoplay, Navigation]}
+                        className="mySwiper"
+                        grabCursor={true}
+                        breakpoints={{
+                            0: {
+                                slidesPerView: 1,
+                                spaceBetween: 10
+                            },
+                            480: {
+                                slidesPerView: 2,
+                                spaceBetween: 10
+                            },
+                            768: {
+                                slidesPerView: 3,
+                                spaceBetween: 15
+                            },
+                            1024: {
+                                slidesPerView: 4,
+                                spaceBetween: 15
+                            },
+                            1280: {
+                                slidesPerView: 5,
+                                spaceBetween: 30
+                            }
+                        }}
+                    >
+                        <SwiperSlide>
+                            <div className={styles.tarjeta}>
+                                <p className={styles.paragraph}>
+                                    <span className={styles.tooltip} mensaje="Html5"><FaHtml5 /></span>
+                                </p>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className={styles.tarjeta}>
+                                <p className={styles.paragraph}>
+                                    <span className={styles.tooltip} mensaje="Css3"><FaCss3Alt /></span>
+                                </p>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className={styles.tarjeta}>
+                                <p className={styles.paragraph}>
+                                    <span className={styles.tooltip} mensaje="Javascript"><FaJs /></span>
+                                </p>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className={styles.tarjeta}>
+                                <p className={styles.paragraph}>
+                                    <span className={styles.tooltip} mensaje="React"><FaReact /></span>
+                                </p>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className={styles.tarjeta}>
+                                <p className={styles.paragraph}>
+                                    <span className={styles.tooltip} mensaje="Laravel"><FaLaravel /></span>
+                                </p>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className={styles.tarjeta}>
+                                <p className={styles.paragraph}>
+                                    <span className={styles.tooltip} mensaje="Mysql"><DiMysql /></span>
+                                </p>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className={styles.tarjeta}>
+                                <p className={styles.paragraph}>
+                                    <span className={styles.tooltip} mensaje="NodeJS"><FaNode /></span>
+                                </p>
+                            </div>
+                        </SwiperSlide>
+                    </Swiper>
                 )}
             </div>
         </div>
--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
     domains: ['res.cloudinary.com'],
+    unoptimized: true,
   },
   env: {
     API_URL: process.env.API_URL,
